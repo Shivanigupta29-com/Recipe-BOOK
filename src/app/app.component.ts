6f2db423fbd4a075358daad93cc6a7a737072144
shivani <shivanigupta290510@gmail.com>
2021-07-06 13:12:26 +0530

    Recipe book

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'recipebook';
+  // loadedFeature = 'recipe' //same name as child name in the event property in HTML file
+  // onNavigate(feature:string){
+  //   this.loadedFeature = feature;
+  // }
+}
